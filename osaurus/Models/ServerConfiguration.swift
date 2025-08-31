309811d9f4fef6e810dc4e1fd951a98737315d79
Terence Pae <terencepae@gmail.com>
2025-08-31 05:50:12 -0700

    added configuration persistence

--- osaurus/Models/ServerConfiguration.swift
+++ osaurus/Models/ServerConfiguration.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 /// Configuration settings for the server
-public struct ServerConfiguration {
+public struct ServerConfiguration: Codable, Equatable {
     /// Server port (1-65535)
     public var port: Int
     
@@ -34,6 +34,58 @@ public struct ServerConfiguration {
     public var genMaxKVSize: Int?
     /// Prefill step size (tokens per prefill chunk)
     public var genPrefillStepSize: Int
+    
+    private enum CodingKeys: String, CodingKey {
+        case port
+        case exposeToNetwork
+        case numberOfThreads
+        case backlog
+        case genTopP
+        case genKVBits
+        case genKVGroupSize
+        case genQuantizedKVStart
+        case genMaxKVSize
+        case genPrefillStepSize
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let defaults = ServerConfiguration.default
+        self.port = try container.decodeIfPresent(Int.self, forKey: .port) ?? defaults.port
+        self.exposeToNetwork = try container.decodeIfPresent(Bool.self, forKey: .exposeToNetwork) ?? defaults.exposeToNetwork
+        self.numberOfThreads = try container.decodeIfPresent(Int.self, forKey: .numberOfThreads) ?? defaults.numberOfThreads
+        self.backlog = try container.decodeIfPresent(Int32.self, forKey: .backlog) ?? defaults.backlog
+        self.genTopP = try container.decodeIfPresent(Float.self, forKey: .genTopP) ?? defaults.genTopP
+        self.genKVBits = try container.decodeIfPresent(Int.self, forKey: .genKVBits)
+        self.genKVGroupSize = try container.decodeIfPresent(Int.self, forKey: .genKVGroupSize) ?? defaults.genKVGroupSize
+        self.genQuantizedKVStart = try container.decodeIfPresent(Int.self, forKey: .genQuantizedKVStart) ?? defaults.genQuantizedKVStart
+        self.genMaxKVSize = try container.decodeIfPresent(Int.self, forKey: .genMaxKVSize)
+        self.genPrefillStepSize = try container.decodeIfPresent(Int.self, forKey: .genPrefillStepSize) ?? defaults.genPrefillStepSize
+    }
+
+    public init(
+        port: Int,
+        exposeToNetwork: Bool,
+        numberOfThreads: Int,
+        backlog: Int32,
+        genTopP: Float,
+        genKVBits: Int?,
+        genKVGroupSize: Int,
+        genQuantizedKVStart: Int,
+        genMaxKVSize: Int?,
+        genPrefillStepSize: Int
+    ) {
+        self.port = port
+        self.exposeToNetwork = exposeToNetwork
+        self.numberOfThreads = numberOfThreads
+        self.backlog = backlog
+        self.genTopP = genTopP
+        self.genKVBits = genKVBits
+        self.genKVGroupSize = genKVGroupSize
+        self.genQuantizedKVStart = genQuantizedKVStart
+        self.genMaxKVSize = genMaxKVSize
+        self.genPrefillStepSize = genPrefillStepSize
+    }
     
     /// Default configuration
     public static var `default`: ServerConfiguration {
